34b14e5d46ee47766684169fef5ad29d16850cb8
wyt <wyt2769194950@shu.edu.cn>
2024-10-02 16:26:57 +0800

    version1.11

--- StringClass/String.cpp
+++ StringClass/String.cpp
@@ -44,7 +44,7 @@ String& String::operator=(const String& s)
 	return x;
 }
 
-int String::GetLen()
+int String::GetLen() const
 {
 	return length;
 }
@@ -54,3 +54,62 @@ String::~String()
 	delete[] str;
 }
 
+void String::Delete(const int& pos)
+{
+	if (length == 0)//查询不到待删字符
+	{
+		throw - 1;
+		return;
+	}
+	int i = 0;
+	String temp(*this);//拷贝构造
+	for ( i = pos; i < length - 1; i++)//空开待删除字符
+	{
+		str[i] = temp[i+1];//[]运算符重载
+	}
+	length = length - 1;
+}
+
+void String::Delete(const int& pos, const char& goal)
+{
+	if (length == 0 || pos == length ||length == 1)//查询不到待删字符
+	{
+		throw - 1;
+		return;
+	}
+	if (str[pos+1] != goal)//待删字符与目标字符不同
+	{
+		throw 'e';
+		return;
+	}
+	Delete(pos + 1);
+	
+}
+
+void String::Delete(const int& pos, const String& goal)
+{
+	if ((length-1-pos) < goal.GetLen())//查询不到足够的待删字符
+	{
+		throw - 1;
+		return;
+	}
+	int flag = 0, i, j = 0;
+	for ( i = pos+1; i < pos+1+goal.GetLen(); i++)//待删除部分与目标串不同
+	{
+		if (str[i] != goal[j])
+		{
+			flag = 1;
+			throw 'e';
+			break;
+		}
+		j++;
+	}
+	if (flag != 1)
+	{
+		for ( i = pos+1; i < pos + 1 + goal.GetLen(); i++)
+		{
+			Delete(i);
+		}
+	}
+
+}
